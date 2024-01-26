420574b690565a2afe670be67e5a269cdfc2498b
서재원 <cinos81@nhnlink.co.kr>
2024-01-26 12:14:58 +0900

    트랩 구현 마무리

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider} from "react-router-dom";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Navigate,
+    Route,
+    RouterProvider
+} from "react-router-dom";
 
+// index 페이지로 강제 리다이렉트하는 트랩은 <Routes /> 로 감싸지 않아야 동작한다
 const router = createBrowserRouter(createRoutesFromElements(
     <>
         <Route path="/a/b/c/d" element={<App/>}/>
+        {/*index 페이지로 강제 리다이렉트하는 트랩을 설치한다*/}
         <Route path="*" element={<Navigate to="/a/b/c/d" replace/>}/>
     </>
+// createRoutesFromElements 함수의 옵션으로 basename 을 넘겨서 시작점을 조정한다.
 ), {basename: '/manipulate-route'})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/manipulate-route'
+  base: '/manipulate-route',
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      }
+    }
+  }
 })
