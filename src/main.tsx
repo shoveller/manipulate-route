bfa0b8243b0964d5f08f464fa503a972544d8ff3
서재원 <cinos81@nhnlink.co.kr>
2024-01-26 09:35:43 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/d" element={<App/>}/>), {basename: '/manipulate-route/a/b/c'})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
+)
